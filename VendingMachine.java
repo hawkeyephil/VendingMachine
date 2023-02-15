c31870287ff2d6b17dbceff0963802473996b7b8
ellidelli <elizabeth.delea@gmail.com>
2023-02-14 19:36:47 -0500

    initial commit

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,5 +1,10 @@
-public class Main {
+public class VendingMachine {
     public static void main(String[] args) {
-        
+        int balance = 0;
+        System.out.println("Welcome");
+        System.out.println("------------------");
+        System.out.println("Vending machine balance: " + balance);
+        System.out.println("------------------");
+
     }
 }
